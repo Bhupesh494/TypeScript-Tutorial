a6ff7456c2e79b54faf9c29a74b1ed0d1d22f2d7
Bhupesh Rajpurohit <bhupeshpurohit494@gmail.com>
2022-02-14 08:35:12 +0530

    completed till 17 Lesson Implemented classes and Interface in a mini Project

---
+++ 99-ImpTopics/Public_Private_&_Readonly.ts
@@ -0,0 +1,27 @@
+// classes
+class Invoice {
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
+
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {}
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+});
